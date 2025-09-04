34caad3f752ef512faeb504b0da55252c0993f6a
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 08:50:51 +0100

    <Tutorials>: Full React Tutorial #11 - Reusing Components

--- tutorials/dojo-blog/src/Home.jsx
+++ tutorials/dojo-blog/src/Home.jsx
@@ -3,14 +3,15 @@ import BlogList from "./BlogList.jsx";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        {title: 'Blog #1', body: 'Lorem Ipsum', author: 'Lorem Ipsum', id: 1},
+        {title: 'Blog #1', body: 'Lorem Ipsum', author: 'Mehmet', id: 1},
         {title: 'Blog #2', body: 'Lorem Ipsum', author: 'Lorem Ipsum', id: 2},
-        {title: 'Blog #3', body: 'Lorem Ipsum', author: 'Lorem Ipsum', id: 3},
+        {title: 'Blog #3', body: 'Lorem Ipsum', author: 'Mehmet', id: 3},
     ]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mehmet')} title="Mehmet's Blogs"/>
         </div>
     );
 }
