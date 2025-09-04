587bab288e1170db3722d1b9f5613e8b8fb1af1e
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 09:05:30 +0100

    <Tutorials>: Full React Tutorial #15 - useEffect

--- tutorials/dojo-blog/src/Home.jsx
+++ tutorials/dojo-blog/src/Home.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import BlogList from "./BlogList.jsx";
 
 const Home = () => {
@@ -8,6 +8,12 @@ const Home = () => {
         {title: 'Blog #3', body: 'Lorem Ipsum', author: 'Mehmet', id: 3},
     ]);
 
+    const [name, setName] = useState("Mehmet");
+
+    useEffect(() => {
+        console.log(name);
+    }, [name])
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -16,6 +22,11 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <p>{name}</p>
+            <button onClick={() => {
+                setName("Tuba");
+            }}>Change Name
+            </button>
         </div>
     );
 }
