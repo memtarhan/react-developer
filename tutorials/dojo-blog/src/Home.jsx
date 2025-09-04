cf0fdd20b491566a267b3a221943b2266bf16f33
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 07:28:51 +0100

    <Tutorials>: Full React Tutorial #7 - Click Events

--- tutorials/dojo-blog/src/Home.jsx
+++ tutorials/dojo-blog/src/Home.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("clicked");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("clicked" + name);
+    }
+
     return (
         <div className="home">
             <h2>Home</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Mehmet')}>
+                Click me again
+            </button>
         </div>
     );
 }
