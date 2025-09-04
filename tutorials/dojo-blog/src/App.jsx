5aaf31e09bcd2a2f13588a199ca2b92747f268e0
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 06:59:12 +0100

    <Tutorials>: Full React Tutorial #4 - Dynamic Values in Templates

--- tutorials/dojo-blog/src/App.jsx
+++ tutorials/dojo-blog/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const title = 'Welcome to the new blog';
+    const likes = 50;
+    const link = 'https://www.google.com';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>{title}</h1>
+            <p>Liked {likes} times</p>
+
+            <p> {10} </p>
+            <p> {"Hello!"} </p>
+            <p> {[1, 2, 3]}</p>
+            <p> {Math.random() * 10} </p>
+
+
+            <a href={ link }>Google</a>
+        </>
+    )
 }
 
 export default App
