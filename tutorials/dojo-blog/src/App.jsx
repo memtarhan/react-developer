8685d804a9af9600ba610414551bdf7e5f42c64f
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 07:07:19 +0100

    <Tutorials>: Full React Tutorial #5 - Multiple Components

--- tutorials/dojo-blog/src/App.jsx
+++ tutorials/dojo-blog/src/App.jsx
@@ -1,22 +1,14 @@
 import './App.css'
+import Navbar from "./Navbar.jsx";
+import Home from "./Home.jsx";
 
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    const link = 'https://www.google.com';
-
     return (
         <>
-            <h1>{title}</h1>
-            <p>Liked {likes} times</p>
-
-            <p> {10} </p>
-            <p> {"Hello!"} </p>
-            <p> {[1, 2, 3]}</p>
-            <p> {Math.random() * 10} </p>
-
-
-            <a href={ link }>Google</a>
+            <Navbar/>
+            <div className="content">
+                <Home></Home>
+            </div>
         </>
     )
 }
