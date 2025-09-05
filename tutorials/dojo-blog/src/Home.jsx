a349b6a4a54a5c05225e71652a681c4ecfafb77e
Mehmet Tarhan <hi@memtarhan.com>
2025-09-05 08:57:21 +0100

    <Tutorials>: Full React Tutorial #19 - Handling Fetch Errors

--- tutorials/dojo-blog/src/Home.jsx
+++ tutorials/dojo-blog/src/Home.jsx
@@ -4,13 +4,25 @@ import BlogList from "./BlogList.jsx";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/blogs")
-                .then(res => res.json())
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw Error("Something went wrong");
+                    }
+                })
                 .then(data => {
                     setBlogs(data)
+                    setLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message)
                     setLoading(false);
                 });
         }, 1000)
@@ -18,6 +30,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            {error && <div> {error}</div>}
             {loading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
