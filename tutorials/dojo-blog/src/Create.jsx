21b47d8b73204c067b8f81926fb7cde31b293353
Mehmet Tarhan <hi@memtarhan.com>
2025-09-06 12:13:10 +0100

    <Tutorials>: Full React Tutorial #27 - Controlled Inputs (forms)

--- tutorials/dojo-blog/src/Create.jsx
+++ tutorials/dojo-blog/src/Create.jsx
@@ -1,7 +1,38 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mehmet');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}/>
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={e => setBody(e.target.value)}/>
+
+                <label>Blog author:</label>
+                <select
+                    required
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}>
+                    <option value="Mehmet">Mehmet</option>
+                    <option value="Tuba">Tuba</option>
+                </select>
+
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
