a741703f8d31db0b47f744d4a3f5c7fc77b2b3da
Mehmet Tarhan <hi@memtarhan.com>
2025-09-12 11:56:57 +0100

    <Professional React Dev>: Learn React - Module 1 - React Facts markup

--- become-a-pro-react-developer/learn-react/module-1/react-facts/src/App.jsx
+++ become-a-pro-react-developer/learn-react/module-1/react-facts/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <img src={reactLogo} className="logo react" alt="React logo"/>
+                <h1>React Facts</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walker</li>
+                    <li>Has well over 200k stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default App
