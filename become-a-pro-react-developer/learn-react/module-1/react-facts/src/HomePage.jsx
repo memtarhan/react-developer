c5a70f23361f2efa0096d7c9bf7a96ade5423f02
Mehmet Tarhan <hi@memtarhan.com>
2025-09-15 21:36:19 +0100

    <Professional React Dev>: Learn React - Module 2 - Organizing Components

--- become-a-pro-react-developer/learn-react/module-1/react-facts/src/HomePage.jsx
+++ become-a-pro-react-developer/learn-react/module-1/react-facts/src/HomePage.jsx
@@ -1,26 +1,13 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import MainContent from "./MainContent.jsx";
 
-const MainComponent = () => {
-    return (
-        <div>
-            <h1>Reasons I am excited...</h1>
-            <ol>
-                <li>
-                    React is a popular library, so I will be able to fit in with all the coolest devs otu there!
-                </li>
-                <li>
-                    I am more likely to get this done!
-                </li>
-            </ol>
-        </div>
-    )
-}
+
 const HomePage = () => {
     return (
         <div>
             <Header/>
-            <MainComponent/>
+            <MainContent/>
             <Footer/>
         </div>
     )
