a1cf79b46b94c3fd80fb8fdd410fdd3c535e94e7
Mehmet Tarhan <hi@memtarhan.com>
2025-09-15 22:01:04 +0100

    <Professional React Dev>: Learn React - Module 2 - ReactFacts Project - Main Content Section

--- become-a-pro-react-developer/learn-react/module-1/react-facts/src/MainContent.jsx
+++ become-a-pro-react-developer/learn-react/module-1/react-facts/src/MainContent.jsx
@@ -1,8 +1,15 @@
 const MainContent = () => {
     return (
-        <div>
-            <h1>Main content...</h1>
-        </div>
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over 200k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
