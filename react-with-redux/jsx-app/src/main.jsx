c01eba90c1c6bc69e00892be171f340dec2c63e1
Mehmet Tarhan <hi@memtarhan.com>
2025-12-05 14:43:47 +0300

    <React with Redux>: custom components

--- react-with-redux/jsx-app/src/main.jsx
+++ react-with-redux/jsx-app/src/main.jsx
@@ -1,33 +1,13 @@
 // 1 - Import React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2 - Get a reference to the div with ID root 
 const el = document.getElementById('root');
 
 // 3 - Tell React to take control of that element 
 const root = ReactDOM.createRoot(el);
-
-// 4 - Create a component 
-function App() {
-  const inputType = "number";
-  const minValue = 5;
-  const maxValue = 10;
-  const message = 'Enter age';
-
-  return (
-  <>
-  <input 
-  type={inputType} 
-  min={minValue} 
-  max={maxValue}
-  list={[1, 2, 3]}
-  style={{color: 'red'}}
-  alt={message}
-  />
-  </>
-  );
-}
 
 // 5 - Show the component on the screen 
 root.render(<App></App>)
